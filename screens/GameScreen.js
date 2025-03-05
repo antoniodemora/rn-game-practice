b924c43b010e524003ec2387163f0aa119151c17
Juan Oyosa <jarodriguezo87@gmail.com>
2025-03-05 09:13:41 -0600

    extract random generation function from component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,19 +3,19 @@ import { StyleSheet, Text , View } from 'react-native';
 import Title from '../components/ui/Title';
 import NumberContainer from '../components/game/NumberContainer';
 
-export default function GameString({userNumber}) {
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+    
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return rndNum;
+    }
+}
+
+export default function GameScreen({userNumber}) {
     const initialGuess = generateRandomBetween(1, 100, userNumber);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
-
-    function generateRandomBetween(min, max, exclude) {
-        const rndNum = Math.floor(Math.random() * (max - min)) + min;
-        
-        if (rndNum === exclude) {
-            return generateRandomBetween(min, max, exclude);
-        } else {
-            return rndNum;
-        }
-    }
 
     return (
         <View style={styles.game}>
